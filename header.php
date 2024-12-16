4028a1d09338a58721c697a43172e9790b8c36cc
Carlos <carlos.leal@penseavanti.com.br>
2024-12-16 13:25:46 -0300

    wip: header

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <header class="header_container bg-red-500 xl:flex sm:w-full xl:h-[104px] justify-between items-center mx-auto container relative z-50">
+  <header class="header_container xl:flex sm:w-full xl:h-[104px] justify-between items-center mx-auto container relative z-50">
     <div>LOGO</div>   
     <nav class="xl:flex xl:items-center">
-      <?php wp_nav_menu( array( 'theme_location' => 'header_menu', 'container_class' => 'header_menu_nav_container' ) ); ?>
+      <?php wp_nav_menu( array( 'theme_location' => 'header_menu', 'container' => 'none' ) ); ?> <!--container: none força a ul como filho do nav --> 
     </nav>
   </header>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+<main class="container mx-auto">
   <p>BODY</p>
+</main>
 </body>
 </html>
