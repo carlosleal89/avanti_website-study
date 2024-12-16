22320bfa00b446f5f2eb68f629b5a823a171d791
Carlos <carlos.leal@penseavanti.com.br>
2024-12-16 19:19:11 -0300

    adiciona video na home

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
-<main class="container mx-auto">
-  <p>BODY</p>
+<main class="min-h-screen">
+  <section class="video-slider-home min-h-screen relative">
+    <video autoplay="autoplay" loop="loop" muted="muted">
+      <source src=<?php echo get_template_directory_uri() . "/assets/banner-home.mp4" ?> type="video/mp4">
+    </video>
+  </section>
 </main>
 </body>
 </html>
