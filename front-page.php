4a652c2f450a8b33a11ef8e1a24ce4091726c58b
Carlos <carlos.leal@penseavanti.com.br>
2024-12-17 11:05:41 -0300

    centraliza o conteudo do banner-hero

--- front-page.php
+++ front-page.php
@@ -1,3 +1,6 @@
-<?php get_template_part('template-parts/banner-hero'); ?>
+<section class="header-banner-hero min-h-screen relative w-full">
+  <?php get_header(); ?>
+  <?php get_template_part( '/template-parts/banner-hero' ); ?>
+</section>
 </body>
 </html>
