5d9127787687a42d489700bcf60feb57f590fbc3
Carlos <carlos.leal@penseavanti.com.br>
2024-12-17 09:33:33 -0300

    criação do arquivo template-parts/banner-hero.php para componentizar esse elemento

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<section class="header-banner-hero min-h-screen relative w-full">
-<?php get_header(); ?>
-  <video class="absolute inset-0 w-full h-full object-cover" autoplay="autoplay" loop="loop" muted="muted">
-    <source src=<?php echo get_template_directory_uri() . "/assets/banner-home.mp4" ?> type="video/mp4">
-  </video>
-  <div class="banner-hero-content z-10 relative flex flex-col justify-center items-center pt-20">
-    <h1>Complete for <span class="text-blue-700">commerce</span></h1>
-    <p class="text-center text-sky-800 pt-6">Impulsione o e-commerce da sua empresa com soluções<br>integradas, performance e experiência.</p>
-    <div class="form-contact-link-container flex pt-6">
-      <a href="#" class="form-contact-link text-center py-3 px-8">Agendar uma apresentação</a>
-    </div>
-  </div>
-</section>
+<?php get_template_part('template-parts/banner-hero'); ?>
 </body>
 </html>
