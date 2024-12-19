3aea6caabcd2d9549afb43fbfa65994f5399c45e
Carlos <carlos.leal@penseavanti.com.br>
2024-12-19 18:03:11 -0300

    conclui primeira versão do form de contato, faltando ajuster no css

--- front-page.php
+++ front-page.php
@@ -2,13 +2,15 @@
   <?php get_header(); ?>
   <?php get_template_part( '/template-parts/banner-hero' ); ?>
 </section>
-<section class="contact-form py-10 md:py-20 px-8 ">
-  <div class="container mx-auto">
+<section id="contact-form" class="contact-form py-10 md:py-20 px-8 ">
+  <div class="container mx-auto px-8">
     <div class="form-contact-title">
-      <h1 class="text-3xl md:text-4x1 mb-4">Quer uma proposta<br> personalizada para a sua empresa?</h1>
+      <h1 class="text-3xl md:text-4xl mb-4 !leading-[50px]">Quer uma proposta<br> personalizada para a sua empresa?</h1>
       <p class="text-grayishblue md:text-lg">Um de nossos Digital Commerce Experts entrará em contato para entender os desafios do seu negócio e conversar sobre como podemos ajudá-lo a acelerar no Comércio Digital.</p>
     </div>
-    <?php echo do_shortcode('[contact-form-7 id="50f70dd" title="Formulário proposta"]'); ?>
+    <div class="contact-form-avanti pt-8">
+      <?php echo do_shortcode('[contact-form-7 id="50f70dd" title="Formulário proposta"]'); ?>
+    </div>
   </div>
 </section>
 <?php get_footer(); ?>
