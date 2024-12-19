0dff5e0e7ecef6aa15dc73965ade75d5b4ddaa55
Carlos <carlos.leal@penseavanti.com.br>
2024-12-19 14:00:02 -0300

    wip: form de contato

--- front-page.php
+++ front-page.php
@@ -2,11 +2,13 @@
   <?php get_header(); ?>
   <?php get_template_part( '/template-parts/banner-hero' ); ?>
 </section>
-<section class="contact-form py-10 md:py-20 px-8">
-  <div class="form-contact-title">
-    <h1 class="text-3xl md:text-4x1 mb-4">Quer uma proposta<br> personalizada para a sua empresa?</h1>
-    <p class="text-grayishblue md:text-lg">Um de nossos Digital Commerce Experts entrará em contato para entender os desafios do seu negócio e conversar sobre como podemos ajudá-lo a acelerar no Comércio Digital.</p>
+<section class="contact-form py-10 md:py-20 px-8 ">
+  <div class="container mx-auto">
+    <div class="form-contact-title">
+      <h1 class="text-3xl md:text-4x1 mb-4">Quer uma proposta<br> personalizada para a sua empresa?</h1>
+      <p class="text-grayishblue md:text-lg">Um de nossos Digital Commerce Experts entrará em contato para entender os desafios do seu negócio e conversar sobre como podemos ajudá-lo a acelerar no Comércio Digital.</p>
+    </div>
+    <?php echo do_shortcode('[contact-form-7 id="50f70dd" title="Formulário proposta"]'); ?>
   </div>
-  <?php echo do_shortcode('[contact-form-7 id="50f70dd" title="Formulário proposta"]'); ?>
 </section>
 <?php get_footer(); ?>
