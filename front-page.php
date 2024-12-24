8b93250cc58ecb38cbb22a9fdfbee6adacb4a724
Carlos <carlos.leal@penseavanti.com.br>
2024-12-23 21:34:40 -0300

    finaliza slider de cases

--- front-page.php
+++ front-page.php
@@ -5,20 +5,20 @@
 <section id="solutions" class="solutions flex justify-center">
   <?php get_template_part( '/template-parts/solutions' ); ?>
 </section>
-<section class="slider-cases overflow-hidden py-10">
+<section class="slider-cases overflow-hidden py-10 px-[2rem] md:pr-0 md:py-20 w-screen">
   <div class="flex flex-col py-10 md:py20 w-screen overflow-visible">
-    <div class="cases-text-slider flex w-screen overflow-visible">
-      <div class="cases-explanation w-[40%] flex flex-col md:items-center lg:items-end pb-8">
+    <div class="cases-text-slider flex flex-col md:gap-x-20 lg:flex-row w-screen overflow-visible">
+      <div class="cases-explanation lg:w-[40%] flex flex-col md:items-center lg:items-end pb-8">
         <h1 class="text-[28px] w-[74%] md:text-5xl mb-4 text-secondary text-start lg:w-3/4">Explore nossos cases de <span class="text-royalblue">sucesso</span></h1>
-        <p class="text-darkishgray mb-6 text-secondary text-start lg:w-3/4">Nossos cases não são apenas exemplos de nosso trabalho, 
+        <p class="text-darkishgray mb-6 text-secondary text-start w-[90%] lg:w-3/4">Nossos cases não são apenas exemplos de nosso trabalho, 
           mas são a representação do impacto tangível que podemos ter no seu negócio. 
           Explore e imagine o que podemos conquistar juntos.</p>
       </div>
-      <div class="w-[60%]">
+      <div class="w-[90%] md:w-[60%]">
         <?php get_template_part( '/template-parts/case-slider' ); ?>
       </div>
     </div>
-    <div class="buttons-dots flex gap-x-4 text-black justify-end width-[50%] ml-[40%] pt-14 pr-[10%]">
+    <div class="buttons-dots flex gap-x-4 text-black justify-end w-[100%] md:w-[50%] my-auto md:ml-[40%] pt-14 pr-[10%]">
       <div class="custom-dots flex w-full"></div>
       <div class="custom-arrows flex gap-x-4"></div>
     </div>
